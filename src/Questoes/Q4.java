cb6d09803852e26faf1a664e3b26ce14eba36ae5
AndrePianco <andrefelipepianco@gmail.com>
2023-08-24 10:51:38 -0300

    Update Q4.java

--- src/Questoes/Q4.java
+++ src/Questoes/Q4.java
@@ -1,15 +1,30 @@
 package Questoes;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q4 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int[] vetorNumeros = new int[10];
+        int Bbl;
 
         for(int i = 0; i < vetorNumeros.length; i++){
             System.out.printf("Informe o %d° valor do vetor", i+1);
             vetorNumeros[i] = scan.nextInt();
+        }
+
+        for(int i = 0; i < vetorNumeros.length-1; i++){
+            for(int j = i+1; j < vetorNumeros.length; j++){
+                if(vetorNumeros[j] < vetorNumeros[i]){
+                    Bbl = vetorNumeros[j];
+                    vetorNumeros[j] = vetorNumeros[i];
+                    vetorNumeros[i] = Bbl;
+                }
+            }
+        }
+
+        for (int k:
+             vetorNumeros) {
+            System.out.printf("%d ",k);
         }
     }
 }
