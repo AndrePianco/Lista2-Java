cfba100a7becab4332a80e12305afa339cfafccc
AndrePianco <andrefelipepianco@gmail.com>
2023-08-24 10:27:46 -0300

    Update Q4.java

--- src/Questoes/Q4.java
+++ src/Questoes/Q4.java
@@ -1,4 +1,15 @@
 package Questoes;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Q4 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int[] vetorNumeros = new int[10];
+
+        for(int i = 0; i < vetorNumeros.length; i++){
+            System.out.printf("Informe o %d° valor do vetor", i+1);
+            vetorNumeros[i] = scan.nextInt();
+        }
+    }
 }
